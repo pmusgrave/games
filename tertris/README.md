56f4776090ea447e1f3b2ae18e4a0df0f61168e1
Paul Musgrave <ps.musgrave@gmail.com>
2020-04-17 14:02:54 -0400

    test rendering
    * render a 20x10 game border
    * test rendering a game piece using ascii char 254 for each square

---
+++ tertris/README.md
@@ -0,0 +1,7 @@
+# TERTRIS
+
+Tetris in the terminal.
+
+This is a learning project, not for commercial use.
+
+<sub><sup><sub><sup>(Please don't sue me).</sup></sub></sup></sub>
--- tertris/src/main.rs
+++ tertris/src/main.rs
@@ -1,10 +1,52 @@
 extern crate ncurses;
 use ncurses::*;
 
+static BOARD_HEIGHT: i32 = 20;
+static BOARD_WIDTH: i32 = 10;
+
 fn main() {
+	setlocale(LcCategory::all, "");
   initscr();
-  printw("hello, world");
+  raw();
+  keypad(stdscr(), true);
+  noecho();
+  curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+
   refresh();
-  getch();
+
+  let mut max_x = 0;
+  let mut max_y = 0;
+  getmaxyx(stdscr(), &mut max_y, &mut max_x);
+
+  let mut start_y = (max_y - BOARD_HEIGHT) / 2;
+  let mut start_x = (max_x - BOARD_WIDTH) / 2;
+  let mut win = create_game_board(0, 0);
+
+  mvwprintw(win, 1, 1, "■");
+  mvwprintw(win, 1, 2, "■");
+  mvwprintw(win, 1, 3, "■");
+  mvwprintw(win, 1, 4, "■");
+  wrefresh(win);
+
+  let mut ch = getch();
+  while ch != KEY_F(1)
+  {
+    ch = getch();
+  }
+
   endwin();
+}
+
+fn create_game_board(x: i32, y: i32) -> WINDOW {
+	let win = newwin(BOARD_HEIGHT, BOARD_WIDTH, y, x);
+	box_(win,0,0);
+	wrefresh(win);
+	win
+}
+
+fn destroy_win(win: WINDOW) {
+  let ch = ' ' as chtype;
+  wborder(win, ch, ch, ch, ch, ch, ch, ch, ch);
+  wrefresh(win);
+  delwin(win);
 }
